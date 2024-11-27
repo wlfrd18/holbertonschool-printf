c14506d39c51bef5237328b45a7e88baa1692e4b
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-27 16:30:44 +0000

    separate the functions of the printf file in others files

---
+++ 1-print_character_and_string.c
@@ -0,0 +1,30 @@
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Print a string.
+ * @s: The string to print.
+ * Return: The number of characters printed.
+ */
+int print_string(char *string)
+{
+	int index = 0;
+
+	if (string == NULL)
+		string = "(null)";
+
+	while (string[index] != '\0')
+		index++;
+	return (write(1, string, index));
+}
+
+/**
+ * print_percent - Print a percent sign.
+ * Return: The number of characters printed (1).
+ */
+int print_percent(void)
+{
+	return (write(1, "%", 1));
+}
---
+++ 2-print_number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/*
+ * print_number - print a number.
+ * @number: number to print
+ * Return: the number of character printed.
+ */
+int print_number(int n)
+{
+	char digit;
+	int count = 0;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+		count++;
+	}
+	if (n / 10)
+	{
+		count = count + print_number(n/10);
+	}
+	digit = (n % 10) + '0';
+
+	write(1, &digit, 1);
+	count++;
+
+	return (count);
+}
