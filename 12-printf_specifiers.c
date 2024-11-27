8761592a999231de28847013c7e0aa51d0aa5570
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-27 18:18:56 +0000

    fix the specifiers functions in one files

--- 12-printf_specifiers.c
+++ 12-printf_specifiers.c
@@ -37,3 +37,29 @@ int print_percent(void)
 {
 	return (write(1, "%", 1));
 }
+
+/**
+ * print_number - Print an integer to standard output.
+ * @number: number to print.
+ * Return: the number of character printed.
+ */
+int print_number(int number)
+{
+	char digit;
+	int count = 0;
+
+	if (number < 0)
+	{
+		count += print_char('-');
+		number = -number;
+	}
+	if (number / 10)
+	{
+		count = count + print_number(number / 10);
+	}
+	digit = (number % 10) + '0';
+
+	count = count + print_char(digit);
+
+	return (count);
+}
